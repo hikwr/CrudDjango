50fb75e6df421a094f71c833ec28c33a1b197c91
Wendler <wendlermoa@gmail.com>
2021-10-07 15:32:46 -0300

    Nome das posições das prateleiras no ForeignKey
    Adicionado nome das posições no ForeignKey

--- produtos/models.py
+++ produtos/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Posicao(models.Model):
     titulo = models.CharField(max_length= 50)
+    
+    def __str__(self):
+        return self.titulo
 
 
 class Produto(models.Model):
