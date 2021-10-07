02265f94f1b2a5d9e1ca4e888ede0b9cf0b6f603
Wendler <wendlermoa@gmail.com>
2021-10-07 15:47:10 -0300

    Adicionado field ManytoMany Field
    Com o manytomany é possivel adicionar novos usuarios para o sistema de administraçao

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Produto, Posicao
+from .models import Produto, Posicao, Usuario
 
 
 admin.site.register(Produto)
 admin.site.register(Posicao)
+admin.site.register(Usuario)
 
 # Register your models here.
--- produtos/models.py
+++ produtos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Posicao(models.Model):
@@ -7,6 +8,10 @@ class Posicao(models.Model):
     def __str__(self):
         return self.titulo
 
+class Usuario (models.Model):
+    usuarios = models.ManyToManyField(User)
+    
+
 
 class Produto(models.Model):
     descricao = models.CharField(max_length=100)
@@ -14,6 +19,7 @@ class Produto(models.Model):
     quantidade = models.IntegerField()
     criado = models.DateField(auto_now_add = True)
     posicao = models.ForeignKey(Posicao, on_delete = models.CASCADE, default= None )
+    usuario = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
