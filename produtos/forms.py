8e19dab2a5c5f0447723bcbb85bfd9bb5268076b
Wendler <wendlermoa@gmail.com>
2021-10-06 23:16:09 -0700

    Adicionado ForeignKey de Produtos
    Adiciconado ForeignKey de Produtos porem ainda nao consegui adicionar o conteudo para o usuario adicionar a prateleira do produto

--- produtos/forms.py
+++ produtos/forms.py
@@ -4,4 +4,10 @@ from .models import Produto
 class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
-        fields = ['descricao', 'preco', 'quantidade']
+        fields = ['descricao', 'preco', 'quantidade', 'posicao']
+        labels = {
+            'descricao' : 'Descrição',
+            'preco' : 'Preço',
+            'quantidade' : 'Quantidade',
+            'posicao' : 'Posição,'
+        }
--- produtos/models.py
+++ produtos/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
 
+class Posicao(models.Model):
+    titulo = models.CharField(max_length= 50)
+
+
 class Produto(models.Model):
     descricao = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=9, decimal_places=2)
     quantidade = models.IntegerField()
     criado = models.DateField(auto_now_add = True)
+    posicao = models.ForeignKey(Posicao, on_delete = models.CASCADE, default= None )
 
     def __str__(self):
         return self.descricao
+
+
+
+
+
 
 
